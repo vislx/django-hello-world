d6fec353d1af39a3c178ea78eabc186f56ffe479
victor <github@vislx.com>
2017-10-31 19:38:37 +0800

    admin

--- polls/admin.py
+++ polls/admin.py
@@ -5,5 +5,21 @@ from django.contrib import admin
 from django.contrib import admin
 from . import models
 
+
+class ChoiceInline(admin.TabularInline):
+    model = models.Choice
+    extra = 3
+
+
+# class ChoiceAdmin(admin.ModelAdmin):
+#     fields = ['question', 'choice_text']
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+
 # Register your models here.
-admin.site.register(models.Question)
+admin.site.register(models.Question, QuestionAdmin)
+# admin.site.register(models.Choice, ChoiceAdmin)
