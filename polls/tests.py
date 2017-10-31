d4d59e270beb3fcf0d4eee4a9c95ef54edb65afa
victor <github@vislx.com>
2017-10-31 16:01:39 +0800

    test

--- polls/tests.py
+++ polls/tests.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.utils import timezone
 from django.test import TestCase
+from .models import  *
+
 
 # Create your tests here.
+# class QuestionModelTest(TestCase):
+    #
+    # def test_question_text_change(self):
+    # #     text = 'hello world'
+    # #     question = Question(question_text=text, pub_date=timezone.now())
+    # #     self.assertEqual(question.question_text, 'hello world2')
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 
@@ -15,7 +15,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse('You are looking at question %s' % question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'views/detail.html', {'question': question})
 
 
 def results(request, question_id):
