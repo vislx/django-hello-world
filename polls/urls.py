96a0bc72249733918668bffb61b4168ab1723ff3
victor <github@vislx.com>
2017-10-31 19:23:35 +0800

    create new views

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail_question'),
-    url(r'^(?P<question_id>[0-9]+)/result/$', views.results, name='result'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail_question'),
+    url(r'^(?P<pk>[0-9]+)/result/$', views.ResultView.as_view(), name='vote_result'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from .models import *
 
 
@@ -14,18 +16,47 @@ def index(request):
     return render(request, 'views/index.html', context)
 
 
-def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'views/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'views/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-    return HttpResponse('You are looking for the result of question %s' % question_id)
+# def detail(request, question_id):
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'views/detail.html', {'question': question})
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'views/detail.html'
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'views/result.html'
+
+
+# def results(request, question_id):
+#     return HttpResponse('You are looking for the result of question %s' % question_id)
 
 
 def vote(request, question_id):
-    return HttpResponse('You are voting for question %s' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'views/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('vote_result', args=(question.id,)))
+
