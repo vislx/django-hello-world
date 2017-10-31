0862dcd49061198f58469c8ba292b1699bb4d707
victor <github@vislx.com>
2017-10-31 14:46:19 +0800

    add template

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/result/$', views.results, name='result'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,25 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('This is poll index page!')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'views/index.html', context)
 
+
+def detail(request, question_id):
+    return HttpResponse('You are looking at question %s' % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse('You are looking for the result of question %s' % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse('You are voting for question %s' % question_id)
